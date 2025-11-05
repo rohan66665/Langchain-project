f8ef87192e1eba2731805a83dcff3293147def89
ROHAN SHARMA <rohan.sharma723@gmail.com>
2025-11-05 19:04:15 +0530

    Final Groq API setup

--- backend_rag.py
+++ backend_rag.py
@@ -5,6 +5,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
+from langchain_community.chains import ConversationalRetrievalChain
 from langchain.chains import ConversationalRetrievalChain
 
 # ✅ Load environment variables
